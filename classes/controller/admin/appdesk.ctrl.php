5df183c6247550d84bfcd72943e11b1c4c879bfd
brousse <brousse@novius.fr>
2013-01-18 14:35:44 +0100

    Première version de la conf global + launcher de test

---
+++ classes/controller/admin/appdesk.ctrl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright 2011 Novius
+ * @license GNU Affero General Public License v3 or (at your option) any later version
+ * http://www.gnu.org/licenses/agpl-3.0.html
+ * @link http://www.novius-os.org
+ */
+
+namespace Lib\Disqus;
+
+class Controller_Admin_Appdesk extends \Nos\Controller_Admin_Appdesk
+{
+    public function action_index($view = null)
+    {
+        return \View::forge('admin/test');
+    }
+}
---
+++ config/metadata.config.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+    'name'    => 'Disqus',
+    'version' => '0.1',
+    'icons' => array(
+        16 => '/static/apps/lib_disqus/images/16-disqus.png',
+        32 => '/static/apps/lib_disqus/images/32-disqus.png',
+        64 => '/static/apps/lib_disqus/images/64-disqus.png',
+    ),
+    'extends' => 'noviusos_comments',
+    'provider' => array(
+        'name' => 'Novius',
+    ),
+    'namespace' => 'Lib\Disqus',
+    'permission' => array(
+    ),
+    'launchers' => array(
+        'disqus_launcher' => array(
+            'name' => 'Disqus',
+            'action' => array(
+                'action' => 'nosTabs',
+                'tab' => array(
+                    'url' => 'admin/lib_disqus/appdesk',
+                )
+            ),
+        ),
+    ),
+    'enhancers' => array(
+    ),
+);
