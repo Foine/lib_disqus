0b86610440ef8340b87e333c0223506f33d6f93b
brousse <brousse@novius.fr>
2013-02-28 14:19:49 +0100

    Ajout de l'appel au dictionnaire dans la vue no_disqus

--- views/front/no_disqus.view.php
+++ views/front/no_disqus.view.php
@@ -1,1 +1,2 @@
+<? Nos\I18n::current_dictionary(array('lib_disqus::default', 'nos::common')); ?>
 <p><?= __('Disqus n\'est pas encore configuré. Rendez vous dans l\'administration pour entrer les informations nécessaires') ?></p>
