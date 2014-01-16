c5f2096eec896e582f9932cc187f525a29a06751
Foine <foineman@gmail.com>
2014-01-16 21:55:38 +0100

    Remove the behaviour commentable actions

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,11 @@ Event::register_function('config|noviusos_comments::metadata', function(&$config
 {
     unset($config['launchers']);
 });
+Event::register_function('config|noviusos_comments::orm/behaviour/commentable', function(&$config)
+{
+    $config['actions']['comments']['visible'] = function($item) { return false; };
+    unset($config['data_mapping']['comments_count']);
+});
 \View::redirect('noviusos_comments::front/list', false, function(&$datas){
     $config = \Lib\Disqus\Controller_Admin_Config::getOptions();
     $config = \Arr::get($config, \Nos\Nos::main_controller()->getContext());
