d601012f37135cc850b3976bd82c3559a8073d18
Foine <foineman@gmail.com>
2014-01-16 21:38:46 +0100

    The disqus identifier is the id for blog post and is the class+id of any other item

--- classes/disqus.php
+++ classes/disqus.php
@@ -3,16 +3,11 @@ namespace Lib\Disqus;
 
 class Disqus {
     static function getIdentifierFromItem(\Nos\Orm\Model $item) {
-        $config = \Lib\Disqus\Controller_Admin_Config::getOptions();
-        $config = \Arr::get($config, \Nos\Nos::main_controller()->getContext());
-        if ($config['update_date'] != '') {
-            return self::_getOldIdentifier($item, $config['update_date']);
-        } 
-        return str_replace('\\', '/',get_class($item)).'/'.$item->get('id');
-    }
-    
-    static protected function _getOldIdentifier(\Nos\Orm\Model $item, $start_date) {
-        //return $item->get('created_at');
-        return $item->get('id');
+        $item_class = get_class($item);
+        if ($item_class == 'Nos\BlogNews\Blog\Model_Post') {
+            return $item->get('id');
+        } else {
+            return str_replace('\\', '/',$item_class).'/'.$item->get('id');
+        }
     }
 }
